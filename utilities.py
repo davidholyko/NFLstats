8df8536f7d8c901ecb801d38aef9b7f28ef289d3
alexander_ko <alexander.ko@airbnb.com>
2018-04-21 21:47:23 -0700

    adding autocomplete

--- utilities.py
+++ utilities.py
@@ -3,12 +3,13 @@
 import csv
 
 def reverse_name(first_last):
+    """Tom Brady -> Brady, Tom"""
     if ' ' in first_last:
         words = first_last.split(' ')
         last_first = words[1] + ', ' + words[0]
         return last_first
     else:
-        return ""        
+        return ""  
 
 def get_qb_id(name):
     with open("NFL QB ID 2016.csv") as f:
@@ -16,6 +17,27 @@ def get_qb_id(name):
         for row in reader:
             if name == row[3] or reverse_name(name) == row[3]: # TODO: make this be case-insensitive
                 return row[2] 
+
+def reverse_reverse_name(last_first):
+    """Brady, Tom -> Tom Brady"""
+    if ',' in last_first:
+        words = last_first.split(', ')
+        first_last = words[1] + ' ' + words[0]
+        return first_last
+    else:
+        return ""                
+
+def get_player_names():
+    player_names = []
+
+    with open("NFL QB ID 2016.csv") as f: # assumes that this is de-duped
+        reader = csv.reader(f)
+        for row in reader: # contains header row
+            name = row[3]
+            player_names.append(name)
+            player_names.append(reverse_reverse_name(name))
+
+    return player_names
 
 def inputQB(qb_id, stat):
     if stat == "yards":
