c5cb6364f856deaec7f98f56136eb16ed32af178
alexander_ko <alexander.ko@airbnb.com>
2018-04-21 21:29:48 -0700

    adding ability to search by name, both first_last and last_first

--- utilities.py
+++ utilities.py
@@ -2,7 +2,22 @@
 
 import csv
 
-def inputQB(QB_name, stat):
+def reverse_name(first_last):
+    if ' ' in first_last:
+        words = first_last.split(' ')
+        last_first = words[1] + ', ' + words[0]
+        return last_first
+    else:
+        return ""        
+
+def get_qb_id(name):
+    with open("NFL QB ID 2016.csv") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if name == row[3] or reverse_name(name) == row[3]: # TODO: make this be case-insensitive
+                return row[2] 
+
+def inputQB(qb_id, stat):
     if stat == "yards":
         file_name = 'NFL 2017 QB Stats Passsing Yards.csv'
     if stat == "TD":
@@ -13,7 +28,7 @@ def inputQB(QB_name, stat):
             for i in range(len(row)):
                 if row[i] == "":
                     row[i] = "0"
-            if QB_name == row[0]:
+            if qb_id == row[0]:
                 return row
 
 # print(inputQB("2495143", "yards"))
